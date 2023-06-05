fa36a426a70469080f925364ed53d9cd6e51bc48
Erik Braga <109657283+ErikBraga@users.noreply.github.com>
2023-06-05 14:57:04 -0300

    exercícios-curso-javascript

---
+++ exercicios-basicos/if.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`A sua velocidade excedeu o limite da via, você foi multado.`)
+}
+console.log('Dirija com cuidado.')
